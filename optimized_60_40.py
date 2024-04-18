d2c27831f3ea8ea84e50bdb822744b987729790b
Sharavsambuu Gunchinish <sharavsambuu@gmail.com>
2024-04-18 16:49:29 +0800

    various rebalancing frequency.

--- optimized_60_40.py
+++ optimized_60_40.py
@@ -160,7 +160,7 @@ rebalancing_cost        = 0.02/100.0
 rebalancing_cost_log    = np.log(1-rebalancing_cost)
 
 previous_rebalance_date = None
-rebalance_frequency     = 'W-MON' # Weekly rebalance
+rebalance_frequency     = '3W' # rebalancing frequency
 stock_weight            = 0.6
 bond_weight             = 0.4
 portfolio_dates         = []
@@ -183,13 +183,34 @@ portfolio_df['cum'             ] = portfolio_df['log_ret'].cumsum().apply(np.exp
 portfolio_df['adjusted_cum'    ] = portfolio_df['adjusted_log_ret'].cumsum().apply(np.exp)
 portfolio_df['adjusted_ret'    ] = portfolio_df['adjusted_cum'].pct_change()
 
-portfolio_df
 
+plt.plot(df['SPY_cum'])
+plt.plot(df['AGG_cum'])
+plt.plot(portfolio_df['adjusted_ret'].cumsum())
+plt.legend(['SPY MA(40,260) performance', 'AGG MA(30,100) performance', f"60/40 portfolio after {round(rebalancing_cost*100.0, 2)}% cost, rebalancing {rebalance_frequency}"])
+plt.show()
+
+#%%
 
 
 #%%
 portfolio_df[['cum', 'adjusted_cum']].plot()
 plt.legend(['Rebalanced portfolio', f"Rebalancing cost {round(rebalancing_cost*100,2)}%"])
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
 
 
 #%%
@@ -221,21 +242,6 @@ qs.plots.yearly_returns(portfolio_df['adjusted_ret'])
 
 #%%
 qs.stats.sharpe(portfolio_df['adjusted_ret'])
-
-
-#%%
-
-
-#%%
-plt.plot(df['SPY_cum'])
-plt.plot(df['AGG_cum'])
-plt.plot(portfolio_df['adjusted_ret'].cumsum())
-plt.legend(['SPY MA(40,260) performance', 'AGG MA(30,100) performance', f"60/40 portfolio after {round(rebalancing_cost*100.0, 2)}% cost performance"])
-plt.show()
-
-
-#%%
-
 
 
 #%%
